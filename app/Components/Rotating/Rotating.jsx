8b558fb0b367d0a6f5b08ab87ece7994cc9a1070
tbrown034 <tbrown034@gmail.com>
2023-08-25 01:08:03 -0400

    added 100 photos, added logic to Rotating.jsx to display next random photo and added to RotatingImage.jsx contain so there's no cropping issues, but the size is different, looks better though, also on rotating. instead of cost images and listing them all out 1-30 (like I first did) or beyond we got this function with the hlep of chat-gpt, but I mostly get it now: const images=array.from ...

--- app/Components/Rotating/Rotating.jsx
+++ app/Components/Rotating/Rotating.jsx
@@ -1,51 +1,41 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import { useState } from "react";
 import RotatingImage from "./RotatingImage";
-import { listAll, getDownloadURL } from "firebase/storage";
-import { imagesRef } from "../../utils/firebase";
 import { RightIcon } from "../Icons/icons";
 
+const images = Array.from({ length: 100 }, (_, i) => {
+  return `/Images/Indy${i + 1}.jpeg`;
+});
+
 export default function Rotation() {
-  const [imageUrls, setImageUrls] = useState([]);
-  const [currentImageUrl, setCurrentImageUrl] = useState("");
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-  // Fetch images and set a random image initially
-  useEffect(() => {
-    async function fetchImages() {
-      const urls = [];
-      const list = await listAll(imagesRef);
-      for (const item of list.items) {
-        const url = await getDownloadURL(item);
-        urls.push(url);
-      }
-      setImageUrls(urls);
-      const randomImage = urls[Math.floor(Math.random() * urls.length)];
-      setCurrentImageUrl(randomImage);
-    }
-    fetchImages();
-  }, []);
+  const handleNextPicture = () => {
+    let randomIndex;
+    do {
+      randomIndex = Math.floor(Math.random() * images.length);
+    } while (randomIndex === currentIndex);
+    setCurrentIndex(randomIndex);
+  };
 
-  // Set a new random image
-  function nextImage() {
-    const randomImage = imageUrls[Math.floor(Math.random() * imageUrls.length)];
-    setCurrentImageUrl(randomImage);
-  }
+  const fileName = `Indy${currentIndex + 1}.jpeg`;
 
   return (
-    <section className="flex flex-col items-center justify-center gap-6 p-6 border-8 border-emerald-600 rounded-2xl ">
+    <section className="flex flex-col items-center justify-center gap-6 p-6 border-8 border-emerald-600 rounded-2xl">
       <h1 className="text-4xl font-bold">Random Indy Pic Generator</h1>
-      <h2 className="text-2xl ">
+      <h2 className="text-2xl">
         Feeling down? Bored? Just want to see an awesome pup? Click away!
       </h2>
 
-      <RotatingImage imageUrl={currentImageUrl} />
+      <RotatingImage src={images[currentIndex]} fileName={fileName} />
       <button
+        onClick={handleNextPicture}
         className="flex items-center justify-center gap-4 p-4 px-8 text-lg bg-emerald-300 rounded-3xl hover:bg-emerald-400 active:bg-emerald-600"
-        onClick={nextImage}
       >
         <p className="text-lg font-semibold">Next Picture</p>
         <RightIcon />
       </button>
+      <p className="text-lg font-medium">{fileName}</p>
     </section>
   );
 }
--- app/Components/Rotating/RotatingImage.jsx
+++ app/Components/Rotating/RotatingImage.jsx
@@ -1,16 +1,13 @@
-import React, { useState, useEffect } from "react";
-import { getStorage, ref, listAll, getDownloadURL } from "firebase/storage";
-import { imagesRef } from "../../utils/firebase";
 import Image from "next/image";
 
-export default function RotatingImage({ setImageUrl, imageUrl }) {
+export default function RotatingImage({ src }) {
   return (
-    <section className="relative w-full h-[500px] overflow-hidden  rounded-3xl ">
+    <section className="relative w-full h-[500px] overflow-hidden rounded-3xl">
       <Image
-        src={imageUrl}
+        src={src}
         alt="Random Indy photo"
-        layout="fill"
-        objectFit="cover"
+        fill={true}
+        style={{ objectFit: "contain" }} // This will make sure the image covers the entire container
       />
     </section>
   );
